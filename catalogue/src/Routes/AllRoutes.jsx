98ee9d2521f1007d831b8cb453e66e243702505c
mahapatrak898 <mahapatrak898.kkm@gmail.com>
2023-07-08 14:47:46 +0530

    v4

--- catalogue/src/Routes/AllRoutes.jsx
+++ catalogue/src/Routes/AllRoutes.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Catalogue from "../components/catalogue";
 import { Details } from "../components/Details";
+import Home from "../components/Home";
 
 export default function AllRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Catalogue />} />
+      <Route path="/" element={<Home />} />
       <Route path="/:name" element={<Details/>} />
     </Routes>
   );
